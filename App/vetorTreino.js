5f7d2e0b331c584c4f37ca9e2c00f035ca05a17d
MATEUS SANTANA DE OLIVEIRA <mateus_oliveira8@estudante.sesisenai.org.br>
2023-11-08 14:12:52 -0300

    Alterado o vetor de treinos

--- App/vetorTreino.js
+++ App/vetorTreino.js
@@ -3,48 +3,48 @@ export const treinoMassa=[
     {
         id: '1',
         nomeExercicio: 'Supino reto',
-        quantidadeRepeticoes: 12,
-        qtdSeries: 4,
+        quantidadeRepeticoes: 'Repetições: 12x',
+        qtdSeries: 'Séries: 4',
         tipoMusculo: 'Peitoral',
         imagem: require('./imagem/supinoReto.gif')
     },
     {
         id: '2',
         nomeExercicio: 'Leg Press',
-        quantidadeRepeticoes: 10,
-        qtdSeries: 4,
+        quantidadeRepeticoes: 'Repetições: 10x',
+        qtdSeries: 'Séries: 4',
         tipoMusculo: 'Quadríceps',
         imagem: require('./imagem/legPress.gif')
     },
     {
         id:'3',
         nomeExercicio: 'Bíceps barra',
-        quantidadeRepeticoes: 12,
-        qtdSeries: 4,
+        quantidadeRepeticoes: 'Repetições: 12x',
+        qtdSeries: 'Séries: 4',
         tipoMusculo: 'Bíceps',
         imagem: require('./imagem/bicepsBarra.webp')
     },
     {
         id: '4',
         nomeExercicio: 'Remada',
-        quantidadeRepeticoes: 12,
-        qtdSeries: 4,
+        quantidadeRepeticoes: 'Repetições: 12x',
+        qtdSeries: 'Séries: 4',
         tipoMusculo: 'Costas',
         imagem: require('./imagem/remadaHalter.gif')
     },
     {
         id: '5',
         nomeExercicio: 'Panturrilha Halteres',
-        quantidadeRepeticoes: 10,
-        qtdSeries: 4,
+        quantidadeRepeticoes: 'Repetições: 10x',
+        qtdSeries: 'Séries: 4',
         tipoMusculo: 'Panturrilha',
         imagem: require('./imagem/panturrilhaHalter.webp')
     },
     {
         id: '6',
         nomeExercicio: 'Tríceps Halter',
-        quantidadeRepeticoes: 12,
-        qtdSeries: 4,
+        quantidadeRepeticoes: 'Repetições: 12x',
+        qtdSeries: 'Séries: 4',
         tipoMusculo: 'Tríceps',
         imagem: require('./imagem/tricepsHalter.gif')
     },
@@ -54,26 +54,26 @@ export const treinoMassa=[
 export const treinoPerderPeso=[
     {
         nomeExercicio: 'Corda Naval',
-        quantidadeRepeticoes: 12,
-        qtdSeries: 4,
+        quantidadeRepeticoes: 'Repetições: 12x',
+        qtdSeries: 'Séries: 4',
         imagem: require('./imagem/cordaNaval.gif')
     },
     {
         nomeExercicio: 'Pular Corda',
-        quantidadeRepeticoes: 12,
-        qtdSeries: 4,
+        quantidadeRepeticoes: 'Repetições: 12x',
+        qtdSeries: 'Séries: 4',
         imagem: require('./imagem/pularCorda.webp')
     },
     {
         nomeExercicio: 'Abdominal',
-        quantidadeRepeticoes: 12,
-        qtdSeries: 4,
+        quantidadeRepeticoes: 'Repetições: 12x',
+        qtdSeries: 'Séries: 4',
         imagem: require('./imagem/abdominal.webp')
     },
     {
         nomeExercicio: 'Polichinelo',
-        quantidadeRepeticoes: 12,
-        qtdSeries: 4,
+        quantidadeRepeticoes: 'Repetições: 12x',
+        qtdSeries: 'Séries: 4',
         imagem: require('./imagem/polichinelo.webp')
     },
     {
